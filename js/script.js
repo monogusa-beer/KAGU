00ba496b5c88d323b784f54f9f626f9786cbb026
楠欣士 <>
2020-04-22 10:23:48 +0900

    kv完成

--- js/script.js
+++ js/script.js
@@ -2,5 +2,13 @@ $(function () {
 	//  object-fit-imagesを呼び出し
 	objectFitImages();
 
+	// mv__listをslick化
+	$('.js-mv__list').slick({
+		adaptiveHeight: true,
+		autoplay: true,
+		dots: true,
+		dotsClass: 'mv__dots'
+	});
+
 
 });
