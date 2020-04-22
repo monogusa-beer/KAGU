a78817cdc844fc1039aab2b99c7788186c192e0d
楠欣士 <>
2020-04-22 19:17:34 +0900

    sp画面完成

--- js/script.js
+++ js/script.js
@@ -2,6 +2,19 @@ $(function () {
 	//  object-fit-imagesを呼び出し
 	objectFitImages();
 
+	// ハンバーガーメニュー
+	$(function () {
+		$('.js-hamburgerButton').click(function () {
+			$('body').toggleClass('is-navActive');
+
+			if ($(this).attr('aria-expanded') == 'false') {
+				$(this).attr('aria-expanded', true);
+			} else {
+				$(this).attr('aria-expanded', false);
+			}
+		});
+	});
+
 	// mv__listをslick化
 	$('.js-mv__list').slick({
 		adaptiveHeight: true,
